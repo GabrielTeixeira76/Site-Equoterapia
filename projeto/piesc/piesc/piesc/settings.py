4a160181ef8d1edbb9ace13403f59cc2094004ca
Gabriel Antonio Teixeira <gantonio835@gmail.com>
2025-03-24 12:19:57 -0300

    atualização 3

--- projeto/piesc/piesc/piesc/settings.py
+++ projeto/piesc/piesc/piesc/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'piesc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'piesc/piesc/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'piesc/paginas/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
